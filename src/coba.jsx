5b4b83e1763dd97b9ddd5a4e9829f763f8d29a6d
WahyuniPutra <wahyunp@mhs.eng.upr.ac.id>
2024-10-30 21:24:20 +0700

    Add Beranda component

---
+++ src/coba.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Beranda = () => {
+    return (
+        <div>
+            <h1>Selamat Datang di Beranda</h1>
+            <p>Ini adalah halaman beranda aplikasi Anda.</p>
+        </div>
+    );
+};
+
+export default Beranda;
