9f53efe9ff33d22c8d5128275dcb726de5334344
WahyuniPutra <wahyunp@mhs.eng.upr.ac.id>
2024-10-31 00:02:57 +0700

    habis nyoba detail, kebalik cok

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './pembeli/produk.jsx';
+import App from './pembeli/detailproduk.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
