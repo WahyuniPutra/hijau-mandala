a9f08b9c51dadc30a1d7177cb1e550a0e127c00e
WahyuniPutra <wahyunp@mhs.eng.upr.ac.id>
2024-11-02 18:47:47 +0700

    call call an

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './pembeli/verifikasi.jsx';
+import App from './pembelinew/beranda.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
